cdde7d8c4f227addcd7c22a50b4b642c2afb0bcd
Helena V. Icart <helenav.icart@gmail.com>
2015-02-24 15:26:49 +0100

    Getting img from rss ffffound

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,8 +3,7 @@ class LinksController < ApplicationController
   def home  
     @zipped_tago = import_rss('http://www.tagoartwork.com/feed/')
     @zipped_dribbble = import_rss_dribbble('https://dribbble.com/shots/popular.rss')
-    
-
+    @zipped_ffffound = import_rss_ffffound('http://feeds.feedburner.com/ffffound/everyone')
   end
 
   def import_rss
@@ -29,6 +28,15 @@ class LinksController < ApplicationController
     @items_link = @items.xpath('//item//link').map{|link| link.inner_text}
     @img_sources = @items.map{|item| Nokogiri::HTML(item.children[3].children[1].content).xpath("//img").first["src"]}
     @items_title.zip(@items_link, @img_sources)
+  end
+
+  def import_rss_ffffound(url)
+    @doc = Nokogiri::XML(open(url))
+    @items = @doc.xpath('//item') 
+    @items_title =  @items.xpath('//item//title').map{|title| title.inner_text}
+    @items_link = @items.xpath('//item//link').map{|link| link.inner_text}
+    @items_sources2 = @items.xpath('//item//description').map{ |img|  Nokogiri::HTML(img.inner_text).xpath("//img").first["src"] }
+    @items_title.zip(@items_link, @items_sources2)
   end
 end
  
