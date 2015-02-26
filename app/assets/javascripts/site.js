9fed44a2c5e7d2548d81f4bb6e8a0d912ad4eebf
Helena V. Icart <helenav.icart@gmail.com>
2015-02-26 22:02:45 +0100

    modals on prehome working

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,6 +3,39 @@
 
 // button dropdown menu 
 var ready;
+
+// modal login
+var login_ready = function() {
+  $("#my_modal").hide();
+  console.log("stuff happening");
+
+  $("#login").on("click", function(event) {
+    event.preventDefault();
+    $("#my_modal").show();
+    console.log("stuff happening");
+  });
+
+  $(".close").on("click", function(event) {
+    $("#my_modal").hide();
+  });
+};
+
+var register_ready = function() {
+    $("#my_modal_reg").hide();
+    console.log("stuff happening");
+
+    $("#register").on("click", function(event) {
+      event.preventDefault();
+      $("#my_modal_reg").show();
+      console.log("stuff happening");
+    });
+
+    $(".close").on("click", function(event) {
+      $("#my_modal_reg").hide();
+    });
+  };
+
+
 ready = function() {
   $(".dropdown-toggle").on("click", function(event){
     console.log("stuff");    
@@ -11,27 +44,10 @@ ready = function() {
   $(document).on("click", function(event) {
     $(".dropdown-menu").hide();
   });
+  login_ready();
+  register_ready();
 };
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
-
-// modal login
-var login_ready = function() {
-  $("#my_modal").hide();
-
-  $("#login").on("click", function(event) {
-    event.preventDefault();
-      console.log("stuff happening");    
-
-    $("#my_modal").show();
-  });
-
-  $(".close").on("click", function(event) {
-    $("#my_modal").hide();
-  });
-};
-
-$(document).ready(login_ready);
-$(document).on('page:load', login_ready);
 
