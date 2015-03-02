67e26d633ecfb0c9facf8a6058c2a504b445928e
Helena V. Icart <helenav.icart@gmail.com>
2015-03-02 17:18:39 +0100

    fixed duplicated column error in migrations folder

--- db/migrate/20150225114239_create_clicked_links.rb
+++ db/migrate/20150225114239_create_clicked_links.rb
@@ -1,8 +1,6 @@
 class CreateClickedLinks < ActiveRecord::Migration
   def change
     create_table :clicked_links do |t|
-      t.integer :user_id
-      t.integer :link_id
 
       t.timestamps null: false
     end
