4ce32b4971f760d8e339455bf8dfd5190163e40c
Helena V. Icart <helenav.icart@gmail.com>
2015-03-28 16:40:20 +0100

    working with omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "343784495811346", "b0cddc600182da7f9fda214411ec472a"
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
